d579a6a4a81752bdcd3cc64708c2aeffe6fef323
wolfbing <liubing.wolf@gmail.com>
2014-10-30 22:53:30 +0800

    text msg

--- wechat/views.py
+++ wechat/views.py
@@ -1,24 +1,40 @@
 
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
 import string
 import hashlib
+import time
+from xml.etree import ElementTree as ET
 
 # Create your views here.
+
+# keys of menu
+NS = 'NS'
+NVS = 'NVS'
+BZNS = 'BZNS'
+BZNVS = 'BZNVS'
+ST = 'ST'
+BX = 'BX'
+JDHG = 'JDHG'
+BSBS = 'BSBS'
+BFX = 'BFX'
+
+# msg type
+TEXT_MSG = "text"
 
 
 # wechat 请求入口
 def main(request):
-    return connect_to_wechat_server(request)
-    #return HttpResponse("request entrance")
+    data = parse_msg(request.body)
+    return text_msg(data)
 
 
 # 接入微信服务器
 def connect_to_wechat_server(request):
-    signature = request.GET['signatue']
+    signature = request.GET['signature']
     ts = request.GET['timestamp']
     nonce = request.GET['nonce']
     echo_str = request.GET['echostr']
@@ -32,6 +48,27 @@ def connect_to_wechat_server(request):
         return HttpResponse(echo_str)
     else:
         return HttpResponse('')
+
+
+def text_msg(request_data):
+    create_time = int(time.time())
+    content = "this is text msg"
+    res_data = {
+        "to_user": request_data['FromUserName'],
+        "from_user": request_data['ToUserName'],
+        "create_time": create_time,
+        "content": content
+    }
+    return render_to_response('text_msg.xml', res_data)
+
+def parse_msg(msg):
+    dict = {}
+    root = ET.fromstring(msg)
+    child_list = root.getchildren()
+    for child in child_list:
+        dict[child.tag] = child.text
+    return dict
+
 
 
 
