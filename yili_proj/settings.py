88d949a011831a5eeae00db98eb4bc9da3e867ea
wolfbing <liubing.wolf@gmail.com>
2014-11-19 00:14:03 +0800

    set system default encoding: utf8

--- yili_proj/settings.py
+++ yili_proj/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
