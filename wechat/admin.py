e0922addaa02be531cffcbf6ed3e09884f5826ea
wolfbing <liubing.wolf@gmail.com>
2014-11-18 23:56:04 +0800

    register staticmedia

--- wechat/admin.py
+++ wechat/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from models import BoysAndGirls, FineFood, Voice
+from models import BoysAndGirls, FineFood, Voice, StaticMedia
 # Register your models here.
 
 admin.site.register(BoysAndGirls)
 admin.site.register(FineFood)
 admin.site.register(Voice)
+admin.site.register(StaticMedia)
