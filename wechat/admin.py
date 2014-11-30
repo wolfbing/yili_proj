0aed7aa3df33fcf59b1ae1193b99e3bd2e824e19
wolfbing <liubing.wolf@gmail.com>
2014-12-01 00:01:38 +0800

    no

--- wechat/admin.py
+++ wechat/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from models import BoysAndGirls, FineFood, Voice, StaticMedia, \
-    FansKLLMedia, FansKLL
+    FansKLLMedia, FansKLL, FansMSTJ, FansMSTJMedia
 # Register your models here.
 
 admin.site.register(BoysAndGirls)
@@ -10,3 +10,5 @@ admin.site.register(Voice)
 admin.site.register(StaticMedia)
 admin.site.register(FansKLL)
 admin.site.register(FansKLLMedia)
+admin.site.register(FansMSTJ)
+admin.site.register(FansMSTJMedia)
