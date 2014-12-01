08ca0ebc76e61346405df3890c89fdf178306be5
wolfbing <liubing.wolf@gmail.com>
2014-12-01 23:14:08 +0800

    no

--- yili_proj/settings.py
+++ yili_proj/settings.py
@@ -59,9 +59,17 @@ WSGI_APPLICATION = 'yili_proj.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    "default": {
+        'ENGINE': "django.db.backends.mysql",
+        'NAME': "wechat",
+        'USER': "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "3306"
     }
 }
 
