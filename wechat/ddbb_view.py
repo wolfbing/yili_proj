c3a78e3e002368fc24d52158cc827cd52cbd3c42
wolfbing <liubing.wolf@gmail.com>
2014-12-14 16:11:34 +0800

    no

--- wechat/ddbb_view.py
+++ wechat/ddbb_view.py
@@ -16,9 +16,23 @@ MR = "MR"
 MRS = "MRS"
 FANSHOW = "FANSHOW"
 
+# request xml中的key
+MSG_TYPE = "MsgType"
+
+# msg type
+TEXT_MSG = "text"
+
+# answer text
+welcome_text = u"Hello~亲爱的早饭，感谢您关注滴滴叭叭早上好，这里每天都会为大家发布各种有趣资讯，" \
+               u"快来和我们一起互动吧！么么哒~~/呲牙"
+
 
 def main(request):
-    return connect_to_wechat_server(request)
+    req_data = views.parse_msg(request.body)
+    if req_data[MSG_TYPE] == TEXT_MSG:
+        return views.text_msg(req_data, welcome_text)
+    else:
+        return HttpResponse("")
 
 
 # 接入微信服务器
