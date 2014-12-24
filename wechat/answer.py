63ec4469f5bc9c83d957e9321b8474eb38fcba6f
wolfbing <liubing.wolf@gmail.com>
2014-12-24 16:14:16 +0800

    no

--- wechat/answer.py
+++ wechat/answer.py
@@ -25,8 +25,8 @@ str_history = u"点击右上角，在进入的页面中就能看到‘查看历�
 str1 = u"点击下面链接了解报名括拉拉档案详情, 很简单的哦~\n" + u"http://121.199.32.77/wechat/attendkll/"
 str2 = u"点击下面链接了解推荐美食的方法, 很简单的, 期待着您的推荐哦~\n" + u"http://121.199.32.77/wechat/recommendfood/"
 str3 = u"点击下面链接了解参加摆饭秀的方法，很简单的~ 舞台很宽敞，期待你来秀！\n" + u"http://127.0.0.1:8000/wechat/fanshow/"
-str4 = u"点击下面链接，欣赏更多女神！\n" + u"http://121.199.32.77/wechat/allnvs/"
-str5 = u"点击下面链接，欣赏更多男神！\n" + u"http://121.199.32.77/wechat/allns/"
+str4 = u"点击下面链接，欣赏更多女神！\n" + u"http://121.199.32.77/mobile/klllist/nvs/1/"
+str5 = u"点击下面链接，欣赏更多男神！\n" + u"http://121.199.32.77/mobile/klllist/ns/1/"
 
 str_suancaiyu = u"店名：天府渔歌；\n地址：南京市洋珠巷玉带园彩霞街菜市场门口"
 str_huntun = u"店名：味真馄饨；\n地址：南京市茶南大街茶南兆园13栋44号103（茶南大街建设银行旁巷内）"
